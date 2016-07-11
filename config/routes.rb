55f41b29438921755dfb2efc1ae5293f8ae3e370
Bryan Perez <bryan@breakthrough.com>
2016-07-11 14:04:17 -0700

    messing around with neat

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :subtasks
   resources :to_dos
   resources :notes
+  root 'notes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
